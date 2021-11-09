02b15ef5106a048d0471a6a7e8f14f23e18e3055
Brady <brady.s.christopher@gmail.com>
2021-11-09 11:17:12 -0600

    Fix cart template

--- templates/woocommerce/cart/cart.php
+++ templates/woocommerce/cart/cart.php
@@ -12,12 +12,10 @@
  *
  * @see     https://docs.woocommerce.com/document/template-structure/
  * @package WooCommerce\Templates
- * @version 4.4.0
+ * @version 3.8.0
  */
 
 defined( 'ABSPATH' ) || exit;
-
-wc_print_notices();
 
 do_action( 'woocommerce_before_cart' ); ?>
 
@@ -27,12 +25,12 @@ do_action( 'woocommerce_before_cart' ); ?>
 	<table class="shop_table shop_table_responsive cart woocommerce-cart-form__contents" cellspacing="0">
 		<thead>
 			<tr>
-				<?php // ThemeFusion edit for Avada theme: change table layout and columns. ?>
+				<th class="product-remove">&nbsp;</th>
+				<th class="product-thumbnail">&nbsp;</th>
 				<th class="product-name"><?php esc_html_e( 'Product', 'woocommerce' ); ?></th>
 				<th class="product-price"><?php esc_html_e( 'Price', 'woocommerce' ); ?></th>
 				<th class="product-quantity"><?php esc_html_e( 'Quantity', 'woocommerce' ); ?></th>
 				<th class="product-subtotal"><?php esc_html_e( 'Subtotal', 'woocommerce' ); ?></th>
-				<th class="product-remove">&nbsp;</th>
 			</tr>
 		</thead>
 		<tbody>
@@ -48,105 +46,6 @@ do_action( 'woocommerce_before_cart' ); ?>
 					?>
 					<tr class="woocommerce-cart-form__cart-item <?php echo esc_attr( apply_filters( 'woocommerce_cart_item_class', 'cart_item', $cart_item, $cart_item_key ) ); ?>">
 
-						<td class="product-name" data-title="<?php esc_attr_e( 'Product', 'woocommerce' ); ?>">
-							<?php // ThemeFusion edit for Avada theme: add thumbnail to product name column. ?>
-							<div class="fusion-product-name-wrapper">
-								<span class="product-thumbnail">
-									<?php
-										$thumbnail = apply_filters( 'woocommerce_cart_item_thumbnail', $_product->get_image(), $cart_item, $cart_item_key );
-
-										if ( ! $product_permalink ) {
-											echo $thumbnail; // PHPCS: XSS ok.
-										} else {
-											printf( '<a href="%s">%s</a>', esc_url( $product_permalink ), $thumbnail ); // PHPCS: XSS ok.
-										}
-									?>
-								</span>
-
-								<div class="product-info">
-									<?php
-									if ( ! $product_permalink ) {
-										echo wp_kses_post( apply_filters( 'woocommerce_cart_item_name',  $_product->get_name(), $cart_item, $cart_item_key ) . '&nbsp;' );
-									} else {
-										echo wp_kses_post( apply_filters( 'woocommerce_cart_item_name', sprintf( '<a href="%s">%s</a>', esc_url( $product_permalink ), $_product->get_name() ), $cart_item, $cart_item_key ) );
-									}
-
-									do_action( 'woocommerce_after_cart_item_name', $cart_item, $cart_item_key );
-
-									// Meta data.
-									echo wc_get_formatted_cart_item_data( $cart_item ); // PHPCS: XSS ok.
-
-									// Backorder notification.
-									if ( $_product->backorders_require_notification() && $_product->is_on_backorder( $cart_item['quantity'] ) ) {
-										echo wp_kses_post( apply_filters( 'woocommerce_cart_item_backorder_notification', '<p class="backorder_notification">' . esc_html__( 'Available on backorder', 'woocommerce' ) . '</p>', $product_id ) );
-									}
-
-									if ( isset( $cart_item['delivery_type'] ) ): ?>
-
-										<div class="cart-field-group">
-											<label for="cart[<?php echo $cart_item_key; ?>][delivery_type]">Delivery Type:&nbsp;&nbsp;</label>
-											<select name="cart[<?php echo $cart_item_key; ?>][delivery_type]" class="delivery-type-select">
-												<option value="one_time" <?php echo $cart_item['delivery_type'] == 'one_time' ? 'selected' : ''; ?>>One Time</option>
-												<option value="regular" <?php echo $cart_item['delivery_type'] == 'regular' ? 'selected' : ''; ?>> Regular</option>
-											</select>
-										</div>
-			
-										<script type="text/javascript">
-			
-											(function() {
-			
-												jQuery('.delivery-type-select').change(function() {
-			
-													if ( jQuery(this).val() == 'regular' ) {
-														jQuery('.frequency-group').show();
-													} else {
-														jQuery('.frequency-group').hide();
-													}
-			
-												});
-			
-											})();
-			
-										</script>
-			
-									<?php endif; ?>
-								</div>
-							</div>
-						</td>
-
-						<td class="product-price" data-title="<?php esc_attr_e( 'Price', 'woocommerce' ); ?>">
-							<?php
-								echo apply_filters( 'woocommerce_cart_item_price', WC()->cart->get_product_price( $_product ), $cart_item, $cart_item_key ); // PHPCS: XSS ok.
-							?>
-						</td>
-
-						<td class="product-quantity" data-title="<?php esc_attr_e( 'Quantity', 'woocommerce' ); ?>"><?php
-							if ( $_product->is_sold_individually() ) {
-								$product_quantity = sprintf( '1 <input type="hidden" name="cart[%s][qty]" value="1" />', $cart_item_key );
-							} else {
-								$product_quantity = woocommerce_quantity_input(
-									array(
-										'input_name'   => "cart[{$cart_item_key}][qty]",
-										'input_value'  => $cart_item['quantity'],
-										'max_value'    => $_product->get_max_purchase_quantity(),
-										'min_value'    => '0',
-										'product_name' => $_product->get_name(),
-									),
-									$_product,
-									false
-								);
-							}
-
-							echo apply_filters( 'woocommerce_cart_item_quantity', $product_quantity, $cart_item_key, $cart_item ); // PHPCS: XSS ok.
-						?></td>
-
-						<td class="product-subtotal" data-title="<?php esc_attr_e( 'Total', 'woocommerce' ); ?>">
-							<?php
-								echo apply_filters( 'woocommerce_cart_item_subtotal', WC()->cart->get_product_subtotal( $_product, $cart_item['quantity'] ), $cart_item, $cart_item_key ); // PHPCS: XSS ok.
-							?>
-						</td>
-
-						<?php // ThemeFusion edit for Avada theme: customize item removal. ?>
 						<td class="product-remove">
 							<?php
 								echo apply_filters( // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
@@ -163,6 +62,125 @@ do_action( 'woocommerce_before_cart' ); ?>
 							?>
 						</td>
 
+						<td class="product-thumbnail">
+						<?php
+						$thumbnail = apply_filters( 'woocommerce_cart_item_thumbnail', $_product->get_image(), $cart_item, $cart_item_key );
+
+						if ( ! $product_permalink ) {
+							echo $thumbnail; // PHPCS: XSS ok.
+						} else {
+							printf( '<a href="%s">%s</a>', esc_url( $product_permalink ), $thumbnail ); // PHPCS: XSS ok.
+						}
+						?>
+						</td>
+
+						<td class="product-name" data-title="<?php esc_attr_e( 'Product', 'woocommerce' ); ?>">
+						<?php
+
+						if ( ! $product_permalink ) {
+							echo wp_kses_post( apply_filters( 'woocommerce_cart_item_name', $_product->get_name(), $cart_item, $cart_item_key ) . '&nbsp;' );
+						} else {
+							echo wp_kses_post( apply_filters( 'woocommerce_cart_item_name', sprintf( '<a href="%s">%s</a>', esc_url( $product_permalink ), $_product->get_name() ), $cart_item, $cart_item_key ) );
+						}
+
+						do_action( 'woocommerce_after_cart_item_name', $cart_item, $cart_item_key );
+
+						// Meta data.
+						echo wc_get_formatted_cart_item_data( $cart_item ); // PHPCS: XSS ok.
+
+						// Backorder notification.
+						if ( $_product->backorders_require_notification() && $_product->is_on_backorder( $cart_item['quantity'] ) ) {
+							echo wp_kses_post( apply_filters( 'woocommerce_cart_item_backorder_notification', '<p class="backorder_notification">' . esc_html__( 'Available on backorder', 'woocommerce' ) . '</p>', $product_id ) );
+						}
+                        
+                        if ( isset( $cart_item['delivery_type'] ) ): ?>
+
+                            <div class="cart-field-group">
+                                <label for="cart[<?php echo $cart_item_key; ?>][delivery_type]">Delivery Type:&nbsp;&nbsp;</label>
+                                <select name="cart[<?php echo $cart_item_key; ?>][delivery_type]" class="delivery-type-select">
+                                    <option value="one_time" <?php echo $cart_item['delivery_type'] == 'one_time' ? 'selected' : ''; ?>>One Time</option>
+                                    <option value="regular" <?php echo $cart_item['delivery_type'] == 'regular' ? 'selected' : ''; ?>> Regular</option>
+                                </select>
+                            </div>
+
+                            <script type="text/javascript">
+
+                                (function() {
+
+                                    jQuery('.delivery-type-select').change(function() {
+
+                                        if ( jQuery(this).val() == 'regular' ) {
+                                            jQuery('.frequency-group').show();
+                                        } else {
+                                            jQuery('.frequency-group').hide();
+                                        }
+
+                                    });
+
+                                })();
+
+                            </script>
+
+                        <?php endif; ?>
+                        
+                        <?php if ( isset( $cart_item['delivery_frequency'] ) ): ?>
+
+                            <div class="cart-field-group frequency-group" <?php echo $cart_item['delivery_type'] == 'one_time' ? 'style="display: none;"' : ''; ?>>
+                                <label for="delivery_frequency">Delivery Frequency:&nbsp;&nbsp;</label>
+                                <select name="cart[<?php echo $cart_item_key; ?>][delivery_frequency]">
+
+                                    <?php
+                                    require_once( SPRO_PLUGIN_DIR . '/public/class-spro-public.php' );
+
+                                    $public = new Spro_Public( 'spro', SPRO_VERSION );
+
+                                    $response_body = $public->spro_get_product( $_product->get_sku() );
+                                    $intervals = $response_body[0]->intervals;
+
+                                    foreach ( $intervals as $interval ): ?>
+                                        <option value="<?php echo $interval; ?>" <?php echo $cart_item['delivery_frequency'] == $interval ? 'selected' : ''; ?>><?php echo $interval; ?></option>
+                                    <?php endforeach; ?>
+
+                                </select>
+                            </div>
+
+                        <?php endif; ?>
+
+						</td>
+
+						<td class="product-price" data-title="<?php esc_attr_e( 'Price', 'woocommerce' ); ?>">
+							<?php
+								echo apply_filters( 'woocommerce_cart_item_price', WC()->cart->get_product_price( $_product ), $cart_item, $cart_item_key ); // PHPCS: XSS ok.
+							?>
+						</td>
+
+						<td class="product-quantity" data-title="<?php esc_attr_e( 'Quantity', 'woocommerce' ); ?>">
+						<?php
+						if ( $_product->is_sold_individually() ) {
+							$product_quantity = sprintf( '1 <input type="hidden" name="cart[%s][qty]" value="1" />', $cart_item_key );
+						} else {
+							$product_quantity = woocommerce_quantity_input(
+								array(
+									'input_name'   => "cart[{$cart_item_key}][qty]",
+									'input_value'  => $cart_item['quantity'],
+									'max_value'    => $_product->get_max_purchase_quantity(),
+									'min_value'    => '0',
+									'product_name' => $_product->get_name(),
+								),
+								$_product,
+								false
+							);
+						}
+
+						echo apply_filters( 'woocommerce_cart_item_quantity', $product_quantity, $cart_item_key, $cart_item ); // PHPCS: XSS ok.
+						?>
+						</td>
+
+						<td class="product-subtotal" data-title="<?php esc_attr_e( 'Subtotal', 'woocommerce' ); ?>">
+							<?php
+								echo apply_filters( 'woocommerce_cart_item_subtotal', WC()->cart->get_product_subtotal( $_product, $cart_item['quantity'] ), $cart_item, $cart_item_key ); // PHPCS: XSS ok.
+							?>
+						</td>
 					</tr>
 					<?php
 				}
@@ -171,7 +189,7 @@ do_action( 'woocommerce_before_cart' ); ?>
 
 			<?php do_action( 'woocommerce_cart_contents' ); ?>
 
-			<tr class="avada-cart-actions">
+			<tr>
 				<td colspan="6" class="actions">
 
 					<?php if ( wc_coupons_enabled() ) { ?>
@@ -192,9 +210,7 @@ do_action( 'woocommerce_before_cart' ); ?>
 			<?php do_action( 'woocommerce_after_cart_contents' ); ?>
 		</tbody>
 	</table>
-
 	<?php do_action( 'woocommerce_after_cart_table' ); ?>
-
 </form>
 
 <?php do_action( 'woocommerce_before_cart_collaterals' ); ?>
@@ -202,15 +218,13 @@ do_action( 'woocommerce_before_cart' ); ?>
 <div class="cart-collaterals">
 	<?php
 		/**
-		 * woocommerce_cart_collaterals hook.
+		 * Cart collaterals hook.
 		 *
 		 * @hooked woocommerce_cross_sell_display
 		 * @hooked woocommerce_cart_totals - 10
 		 */
-	 	do_action( 'woocommerce_cart_collaterals' );
+		do_action( 'woocommerce_cart_collaterals' );
 	?>
 </div>
 
-<?php do_action( 'woocommerce_after_cart' );
-
-/* Omit closing PHP tag to avoid "Headers already sent" issues. */
+<?php do_action( 'woocommerce_after_cart' ); ?>
